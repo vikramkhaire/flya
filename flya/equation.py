0121c6919580f17067007806c5b9dcd34a50e5e0
vikramkhaire <vickykhaire@gmail.com>
2023-01-13 13:17:45 +0530

    checks

--- flya/equation.py
+++ flya/equation.py
@@ -14,7 +14,21 @@ c = const.c.to(u.cm/u.s).value # c in cm/s
 alpha_A = 4.2e-13 # recombination coefficient of H at T= 10^4 K
 Mpc_to_cm = u.Mpc.to(u.cm) # 3.085677581467192e+24
 G = const.G.to(u.cm**3/u.g/u.s**2).value # G value
+
+# few values
 rho_crit = 3* (100* u.km.to(u.cm)/Mpc_to_cm)**2/(8*np.pi*G)
 print('Rho crit', rho_crit, '* h^2')
+nu_lya =  c/lya
+
+A =  np.pi * e**2 * f_alpha * alpha_A * rho_crit**2 / (m_e*nu_lya*m_p**2)
+print('A = ', A)
+
+# check
+val = A*4**6*1.1667*(1-0.25)**2*0.0125**2/1e-12/(100* u.km.to(u.cm)/Mpc_to_cm)
+print(val)
+val = A*1.1667*(1-0.2454)**2*0.0224**2/1e-14/(100* u.km.to(u.cm)/Mpc_to_cm)
+print(val)
 
 # formula
+
+
