86831c15ea04210d5ce1e12b8daad81a7a7cf838
vikramkhaire <vickykhaire@gmail.com>
2023-01-21 12:47:51 +0530

    plotting

--- flya/plot_sim_gas_phase.py
+++ flya/plot_sim_gas_phase.py
@@ -10,7 +10,7 @@ plt.rc('font', **font)
 mpl.rcParams['axes.linewidth'] = 1.5
 
 out_fig_name = 'gas_phase.pdf'
-figure_size = [7, 6]
+figure_size = [7, 7]
 fig, ax = plt.subplots(1, 1, figsize=(figure_size[0], figure_size[1]))
 
 path = '/home/vikram/flya/flya/data'
@@ -34,10 +34,12 @@ ax.set_ylabel(r'f$_{\rm Ly \alpha}$')
 ax.set_xlabel(r'$\Delta$')
 
 ax.legend( loc = 'best', fontsize = 10)
-ax.set_ylim(0, 1)
+ax.set_ylim(0.15, 0.9)
 # decorating the plot
 for axis in ['top', 'bottom', 'left', 'right']:
     ax.spines[axis].set_linewidth(1.5)
+
+ax.grid()
 
 fig.savefig(out_fig_name, bbox_inches='tight')
 
