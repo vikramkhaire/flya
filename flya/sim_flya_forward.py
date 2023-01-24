15b6847897b4a6569aa0c178278e565045a81efe
vikramkhaire <vickykhaire@gmail.com>
2023-01-24 14:33:28 +0530

    testing on fwd models

---
+++ flya/sim_flya_forward.py
@@ -0,0 +1,145 @@
+#a simple equation to find the gas in diffuse Lyman alpha fraction
+import numpy as np
+import astropy.constants as const
+import astropy.units as u
+import astropy.table as tab
+
+#---- function to calculte the H(z)
+def Hz_flat(O_m, O_lambda, H0, z = 0.1):
+    """
+    H0 in std units km/s/Mpc
+    """
+    Mpc_to_cm = u.Mpc.to(u.cm)  # 3.085677581467192e+24
+
+    hz = H0*u.km.to(u.cm)/Mpc_to_cm *np.sqrt(O_lambda + O_m*(1+z)**3)
+    hz_100 = 100*hz/H0 # in units of h
+
+    return hz, hz_100
+
+
+def get_tau_avg (beta, forward_file, tau_limits):
+
+    # read tau data
+    data = tab.Table.read(forward_file, hdu = 2)
+    flux  = data['Flux']
+    flux [flux< 0.02] = 0.018 # since -ln (0.018) ~4
+    tau = -np.log(flux)
+
+    # sort
+    tau[tau<tau_limits[0]] = 0
+    tau[tau>tau_limits[1]] = tau_limits[1]
+
+    tau_avg = np.mean(tau**(1/beta))
+
+    return tau_avg
+
+
+
+def diffuse_lya_fraction_forward(taufile, forward_file, Gamma_HI = None, simname = 'tng', tau_def = 'Dave'):
+
+    if tau_def == 'Dave':
+        tau_limits = [0.03, 4]
+        print('Assuming Dave et al 2010 definitoin')
+    else: # Smith
+        tau_limits = [0.015, 4]
+        print('Assuming Shull and Smith definition')
+
+    # constants
+    e = 4.8032e-10  # in esu
+    f_alpha = 0.416  # oscillator strength for Lyman alpha
+    m_e = const.m_e.to(u.g).value  # electron mass 9.1093837015e-28 grams
+    m_p = const.m_p.to(u.g).value  # proton mass 9.1093837015e-28 grams
+    lya = 1.21567e-5  # cm Lyman alpha wavelength
+    c = const.c.to(u.cm / u.s).value  # c in cm/s
+    Mpc_to_cm = u.Mpc.to(u.cm)  # 3.085677581467192e+24
+    alpha_A = 4.2e-13  # recombination coefficient of H at T= 10^4 K
+    G = const.G.to(u.cm ** 3 / u.g / u.s ** 2).value  # G value
+
+    # few values
+    rho_crit = 3 * (100 * u.km.to(u.cm) / Mpc_to_cm) ** 2 / (8 * np.pi * G)
+    #print('Rho crit', rho_crit, '* h^2')
+    nu_lya = c / lya  # Hz Lyman alpha frequency
+
+    A = np.pi * e ** 2 * f_alpha * alpha_A * rho_crit ** 2 / (m_e * nu_lya * m_p ** 2)
+    #print('A = ', A)
+
+    # read cosmology parameters
+    cosmo = tab.Table.read(taufile, hdu = 1)
+
+    z = cosmo['z'][0]
+    #print('z', z)
+    O_m = cosmo['Om0'][0]
+    O_lambda = cosmo['Ode0'][0]
+    h = cosmo['lit_h'] [0]
+    O_bh2 = h**2 * cosmo['Ob0'] [0]
+    H0 = h *100
+    y_p = 1 - cosmo['X'] [0]
+    # electron density correction
+    kHe = (2 - y_p) / (2 - 2 * y_p)
+
+    if Gamma_HI == None:
+        Gamma_HI = 1e-12* cosmo['GAMMA'][0]
+    #print('Gamma_HI', Gamma_HI, 'Obh2', O_bh2, 'yp', y_p, 'lambda', O_lambda)
+
+    if simname == 'tng':
+        T0 = 4038  # K
+        gamma = 1.53
+    if simname == 'ill':
+        T0 = 4239  # K
+        gamma = 1.57
+
+    # note the recombination coeffient has been taken to scale with T^{-0.7}
+    beta = 2 - 0.7 * (gamma - 1)
+
+    tau_avg  = get_tau_avg(beta=beta, forward_file=forward_file, tau_limits=tau_limits)
+
+
+    #formula
+    hz, hz_100 = Hz_flat(O_m = O_m, O_lambda = O_lambda, H0 = H0, z=z)
+    fLya = (Gamma_HI*hz/(A*kHe))**0.5 * tau_avg**(beta/2) *(T0/10000)**(0.35) / ( O_bh2 * (1-y_p) * (1+z)**3 )
+
+    print(fLya, tau_avg, taufile)
+
+    return fLya
+
+"""
+
+def prep_input(taufile, Gamma12,  simname):
+    print('for', simname, 'Gamma_12', Gamma12)
+    x = np.arange(10) + 1
+
+    for tau_low in list([0, 0.01, 0.05, 0.1]):
+        file_name = 'dflya_{}_Gamma_{:0.3f}_taulim_{:0.2f}_x.fits'.format(simname, Gamma12, tau_low)
+
+        flya_array = []
+        for i in x:
+            fraction = diffuse_lya_fraction(taufile=taufile, simname= simname, tau_limits=[tau_low, i])
+            flya_array.append(fraction)
+
+        res = tab.Table([x, flya_array], names=('tau', 'frac'))
+        res.write(file_name, overwrite=True)
+        print(res)
+
+
+
+simname = 'ill'
+path = '/mnt/quasar/vikram/Illustris_z01/old_Illustris/get_Gamma_HI'
+taufile = path + '/' +'ran_skewers_01_random_OVT_tau_Gamma_0.05000_Nran_010000_seed_42.fits'
+prep_input(taufile= taufile, Gamma12= 0.05, simname= simname)
+
+taufile = path + '/' +'ran_skewers_01_random_OVT_tau_Gamma_0.07500_Nran_010000_seed_42.fits'
+prep_input(taufile= taufile, Gamma12= 0.075, simname= simname)
+
+taufile = path + '/' +'ran_skewers_01_random_OVT_tau_Gamma_0.10000_Nran_010000_seed_42.fits'
+prep_input(taufile= taufile, Gamma12= 0.10, simname= simname)
+
+"""
+
+Gamma_12 = 0.05
+path = '/mnt/quasar/vikram/Illustris_z01/get_Gamma_HI'
+tau_file = path + '/' + 'ran_skewers_01_random_OVT_tau_Gamma_{:0.5f}_Nran_010000_seed_42.fits'.format(Gamma_12)
+SN =100
+fwd_file = path + '/flya' + '/forward_model_igmSN_{:0.0f}_res_cos_LP1.fits'.format(SN)
+
+flya = diffuse_lya_fraction_forward(taufile=tau_file, forward_file=fwd_file)
+print(flya, 'TNG')
