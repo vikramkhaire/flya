865932883a2473a0160f48c2fc5b552f00843640
vikramkhaire <vickykhaire@gmail.com>
2023-01-24 12:42:12 +0530

    making forward models to check tau

---
+++ flya/forward_model_igm_cos.py
@@ -0,0 +1,104 @@
+#------------------
+# for reducing  numpy threads
+import os
+os.environ["NUMEXPR_NUM_THREADS"] = "20"
+os.environ["OMP_NUM_THREADS"] = "20"
+os.environ["VECLIB_MAXIMUM_THREADS"] = "20"
+os.environ["OPENBLAS_NUM_THREADS"] = "20"
+os.environ["MKL_NUM_THREADS"] = "20"
+#-----------------
+import matplotlib as mpl
+mpl.use("Agg")
+from enigma.whim.forward_model import forward_model
+os.nice(10)
+
+def run_forward (simname, outfileFirstName = 'igm', zsim =0.1, dz_total = 1):
+
+    # the photoinoization rate in units 10^-12 s^-1 # see notes for choosing this value
+    Gamma_12 = 0.05
+
+    if simname == 'tng':
+        # simulation file
+        hdf5file = '/mnt/quasar/sims/IllustrisTNG/TG100-1/z01/gadget_format/grid/Ill_TG100-1_z0.10.h5'
+        # path to catalogfile
+        path = '/mnt/quasar/vikram/Illustris_z01'
+
+    if simname == 'ill':
+        # simulation file
+        hdf5file = '/mnt/quasar/sims/Illustris/Illustris1/z0.1/gadget_format/grid/Ill1_z0.10.h5'
+        # path to catalogfile
+        path = '/mnt/quasar/vikram/Illustris_z01/old_Illustris'
+
+
+    outpath = path + '/get_Gamma_HI'
+
+    file = outpath + '/' + 'ran_skewers_01_random_OVT_tau_Gamma_{:0.5f}_Nran_010000_seed_42.fits'.format(Gamma_12)
+    print('from:', file)
+
+    model, res = forward_model.model_readin(taufile=file)
+
+    #------------ find number of spectra
+    # assigning box and cosmology parameters
+    ncell = params['Ng'][0]
+    OmegaM = params['Om0'][0]
+    OmegaL = params['Ode0'][0]
+    OmegaB = params['Ob0'][0]
+    h = params['lit_h'][0]
+    Lbox = params['Lbox'][0]
+    Lbox *= u.Mpc
+    # not using the astropy units
+    if zsim is None:
+        z = params['z'][0]
+    else:
+        z = zsim
+
+    # Computing Velocity Data Points
+    cosmo = FlatLambdaCDM(H0=100.0 * h, Om0=OmegaM, Ob0=OmegaB)
+    hubblez = cosmo.H(z)
+    distance = Lbox / h
+    dz = ((distance / ncell) * hubblez / c).value
+
+    nmodel = int(dz_total//dz +1)
+    print ('need {} models to get total dz {}'.fromat(nmodel, dz_total))
+    #--------------------------------------------
+
+    for SN in [35, 70, 100]:
+        print('running for SN', SN)
+        model_all_effects, model_in_lengthened_only, res_array, ind = forward_model.forward_model(
+            model=model, data_resolution=res, nmodel_spectra=nmodel, SN=SN)
+
+        # storing  the forward models
+        # setting names for output dir
+        outpathname = outpath + '/flya'  # + '/' + 'linefitting_forward_model_SN_{:03d}'.format(int(SN))
+
+        if not os.path.exists(outpathname):
+            print('creating: ', outpathname)
+            os.mkdir(outpathname)
+
+        outfilename = outpathname + '/forward_model_' + outfileFirstName + 'SN_{:0.0f}_res_cos_LP1.fits'.format(SN)
+
+        """
+        print('stroring in dir: ', outpathname)
+        if not os.path.isdir(outpathname):
+            os.mkdir(outpathname)
+            print('creating dir: ', outpathname)
+            forward_model.store_spectra_for_vpfitting(model_all_effects=model_all_effects, res_array=res_array,
+                outpath=outpathname, outfile=outfileFirstName)
+        else:
+            if  os.path.exists(outfilename):
+                print('file exists {} : doing no calculations'.format(outfilename))
+            else:
+                forward_model.store_spectra_for_vpfitting(model_all_effects=model_all_effects, res_array=res_array,
+                    outpath=outpathname, outfile=outfileFirstName)
+        """
+
+    return
+
+
+
+SimNameArray= ['ill', 'tng']
+#--------------multiprocessing routine
+import multiprocessing as mp
+pool = mp.Pool(processes=2)
+results = [pool.apply_async(run_forward, args=(simulation, )) for  simulation in SimNameArray]
+output = [p.get() for p in results]
