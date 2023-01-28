7b2b55e69c61d3cf0246137f376239d959040d22
vikramkhaire <vickykhaire@gmail.com>
2023-01-28 20:48:23 +0530

    z = 0.03

--- flya/dlyaf_with_taumin.py
+++ flya/dlyaf_with_taumin.py
@@ -5,7 +5,7 @@ import astropy.units as u
 import astropy.table as tab
 
 #---- function to calculte the H(z)
-def Hz_flat(O_m, O_lambda, H0, z = 0.1):
+def Hz_flat(O_m, O_lambda, H0, z = 0.03):
     """
     H0 in std units km/s/Mpc
     """
@@ -16,7 +16,7 @@ def Hz_flat(O_m, O_lambda, H0, z = 0.1):
 
     return hz, hz_100
 
-def diffuse_lya_fraction(taufile, Gamma_HI = None, simname = 'tng', tau_limits =[0.1, 4]):
+def diffuse_lya_fraction(taufile, Gamma_HI = None, simname = 'tng', tau_limits =[0.1, 4], z_data=0.03):
 
     # constants
     e = 4.8032e-10  # in esu
@@ -41,7 +41,7 @@ def diffuse_lya_fraction(taufile, Gamma_HI = None, simname = 'tng', tau_limits =
     cosmo = tab.Table.read(taufile, hdu = 1)
 
     z = cosmo['z'][0]
-    #print('z', z)
+    print('z', z)
     O_m = cosmo['Om0'][0]
     O_lambda = cosmo['Ode0'][0]
     h = cosmo['lit_h'] [0]
@@ -55,12 +55,22 @@ def diffuse_lya_fraction(taufile, Gamma_HI = None, simname = 'tng', tau_limits =
         Gamma_HI = 1e-12* cosmo['GAMMA'][0]
     #print('Gamma_HI', Gamma_HI, 'Obh2', O_bh2, 'yp', y_p, 'lambda', O_lambda)
 
-    if simname == 'tng':
-        T0 = 4038  # K
-        gamma = 1.53
-    if simname == 'ill':
-        T0 = 4239  # K
-        gamma = 1.57
+    if z_data==0.03:
+        if simname=='tng':
+            T0 =  3852 # K
+            gamma = 1.53
+        if simname=='ill':
+            T0 = 3888  # K
+            gamma = 1.55
+
+    if z_data = 0.1:
+        if simname=='tng':
+            T0 = 4010  # K
+            gamma = 1.527
+        if simname=='ill':
+            T0 = 4231  # K
+            gamma = 1.56
+
 
     # note the recombination coeffient has been taken to scale with T^{-0.7}
     beta = 2 - 0.7 * (gamma - 1)
@@ -84,16 +94,16 @@ def diffuse_lya_fraction(taufile, Gamma_HI = None, simname = 'tng', tau_limits =
     return fLya
 
 
-def prep_input(taufile, Gamma12,  simname):
+def prep_input(taufile, Gamma12,  simname, z_data = 0.03):
     print('for', simname, 'Gamma_12', Gamma12)
     tau_array = np.arange(51)*0.001
 
     # tau max = 4
     taumax= 4
-    file_name = 'taumin_{}_Gamma_{:0.3f}_taumax_4.fits'.format(simname, Gamma12)
+    file_name = 'taumin_{}_Gamma_{:0.3f}_taumax_4_z_{:0.2f}.fits'.format(simname, Gamma12, z_data)
     flya_array = []
     for tau_low in tau_array:
-        fraction = diffuse_lya_fraction(taufile=taufile, simname= simname, tau_limits=[tau_low, taumax])
+        fraction = diffuse_lya_fraction(taufile=taufile, simname= simname, tau_limits=[tau_low, taumax], z_data=z_data)
         flya_array.append(fraction)
 
     res = tab.Table([tau_array, flya_array], names=('taumin', 'frac'))
@@ -102,10 +112,10 @@ def prep_input(taufile, Gamma12,  simname):
 
     # tau max = 5
     taumax= 5
-    file_name = 'taumin_{}_Gamma_{:0.3f}_taumax_5.fits'.format(simname, Gamma12)
+    file_name = 'taumin_{}_Gamma_{:0.3f}_taumax_5_z_{:0.2f}.fits'.format(simname, Gamma12, z_data)
     flya_array = []
     for tau_low in tau_array:
-        fraction = diffuse_lya_fraction(taufile=taufile, simname= simname, tau_limits=[tau_low, taumax])
+        fraction = diffuse_lya_fraction(taufile=taufile, simname= simname, tau_limits=[tau_low, taumax], z_data=z_data)
         flya_array.append(fraction)
 
     res = tab.Table([tau_array, flya_array], names=('taumin', 'frac'))
@@ -115,35 +125,38 @@ def prep_input(taufile, Gamma12,  simname):
     return
 
 
+z_data = 0.03
+zstr = 'z003'
+seed = 1
 
 simname = 'ill'
 path = '/mnt/quasar/vikram/Illustris_z01/old_Illustris/get_Gamma_HI'
 
-taufile = path + '/' +'ran_skewers_01_random_OVT_tau_Gamma_0.01000_Nran_010000_seed_42.fits'
-prep_input(taufile= taufile, Gamma12= 0.01, simname= simname)
+taufile = path + '/' +'ran_skewers_'+zstr+'_random_OVT_tau_Gamma_0.01000_Nran_010000_seed_{}.fits'.format(seed)
+prep_input(taufile= taufile, Gamma12= 0.004, simname= simname, z_data=z_data)
 
-taufile = path + '/' +'ran_skewers_01_random_OVT_tau_Gamma_0.05000_Nran_010000_seed_42.fits'
-prep_input(taufile= taufile, Gamma12= 0.05, simname= simname)
+taufile = path + '/' +'ran_skewers_'+zstr+'_random_OVT_tau_Gamma_0.05000_Nran_010000_seed_{}.fits'.format(seed)
+prep_input(taufile= taufile, Gamma12= 0.007, simname= simname, z_data=z_data)
 
-taufile = path + '/' +'ran_skewers_01_random_OVT_tau_Gamma_0.07500_Nran_010000_seed_42.fits'
-prep_input(taufile= taufile, Gamma12= 0.075, simname= simname)
+taufile = path + '/' +'ran_skewers_'+zstr+'_random_OVT_tau_Gamma_0.07500_Nran_010000_seed_{}.fits'.format(seed)
+prep_input(taufile= taufile, Gamma12= 0.04, simname= simname, z_data=z_data)
 
-taufile = path + '/' +'ran_skewers_01_random_OVT_tau_Gamma_0.10000_Nran_010000_seed_42.fits'
-prep_input(taufile= taufile, Gamma12= 0.10, simname= simname)
+taufile = path + '/' +'ran_skewers_'+zstr+'_random_OVT_tau_Gamma_0.10000_Nran_010000_seed_{}.fits'.format(seed)
+prep_input(taufile= taufile, Gamma12= 0.07, simname= simname, z_data=z_data)
 
 
 simname = 'tng'
-path = '/mnt/quasar/vikram/Illustris_z01/get_Gamma_HI'
+path = '/mnt/quasar/vikram/Illustris_z003/get_Gamma_HI'
 
-taufile = path + '/' +'ran_skewers_01_random_OVT_tau_Gamma_0.01000_Nran_010000_seed_42.fits'
-prep_input(taufile= taufile, Gamma12= 0.01, simname= simname)
+taufile = path + '/' +'ran_skewers_'+zstr+'_random_OVT_tau_Gamma_0.01000_Nran_010000_seed_{}.fits'.format(seed)
+prep_input(taufile= taufile, Gamma12= 0.004, simname= simname, z_data=z_data)
 
-taufile = path + '/' +'ran_skewers_01_random_OVT_tau_Gamma_0.05000_Nran_010000_seed_42.fits'
-prep_input(taufile= taufile, Gamma12= 0.05, simname= simname)
+taufile = path + '/' +'ran_skewers_'+zstr+'_random_OVT_tau_Gamma_0.05000_Nran_010000_seed_{}.fits'.format(seed)
+prep_input(taufile= taufile, Gamma12= 0.007, simname= simname, z_data=z_data)
 
-taufile = path + '/' +'ran_skewers_01_random_OVT_tau_Gamma_0.07500_Nran_010000_seed_42.fits'
-prep_input(taufile= taufile, Gamma12= 0.075, simname= simname)
+taufile = path + '/' +'ran_skewers_'+zstr+'_random_OVT_tau_Gamma_0.07500_Nran_010000_seed_{}.fits'.format(seed)
+prep_input(taufile= taufile, Gamma12= 0.04, simname= simname, z_data=z_data)
 
-taufile = path + '/' +'ran_skewers_01_random_OVT_tau_Gamma_0.10000_Nran_010000_seed_42.fits'
-prep_input(taufile= taufile, Gamma12= 0.10, simname= simname)
+taufile = path + '/' +'ran_skewers_'+zstr+'_random_OVT_tau_Gamma_0.10000_Nran_010000_seed_{}.fits'.format(seed)
+prep_input(taufile= taufile, Gamma12= 0.07, simname= simname, z_data=z_data)
 
